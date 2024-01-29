2019a529d77562f36276db26135140c573b0d2ed
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2024-01-29 18:20:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  makeSound() {
+    console.log("Some generic animal sound");
+  }
+}
+
+class Dog extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  bark() {
+    console.log("Woof");
+  }
+
+  // Overriding the makeSound method for Dog
+  makeSound() {
+    console.log("Woof woof");
+  }
+}
+
+class Cat extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  purr() {
+    console.log("Purr");
+  }
+
+  // Overriding the makeSound method for Cat
+  makeSound() {
+    console.log("Meow");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
